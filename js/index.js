f04ec4a5e8b2b7b14e9aff6c49b5b9db415af48e
Diegofergamboa <diegofergamboa@hotmail.es>
2021-12-10 17:51:55 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -17,7 +17,27 @@ function getProductsDb () {
     });
 }
 async function loadProducts() {
-    console.log("Función loadProducts ejecutada correctamente");
     const products = await getProductsDb();
-    console.log(products);
+    
+    let html = '';
+    products.forEach(product => {
+        html += `
+            <div class="col-3 product-container">
+                <div class="card product">
+                    <img 
+                    src="${product.image}"
+                    class="card-img-top"
+                    alt="${product.name}"
+                    />
+                    <div class="card-body">
+                        <h5 class="card-title">${product.name}</h5>
+                        <p class="card-text">${product.extraInfo}</p>
+                        <p class="card-text">${product.price} $ / Unidad</p>
+                        <button type="button" class="btn btn-primary btn-cart">Añadir al carrito</button>
+                    </div>
+                </div>
+            </div>
+        `
+    });
+    document.getElementsByClassName("products")[0].innerHTML = html;
 }
